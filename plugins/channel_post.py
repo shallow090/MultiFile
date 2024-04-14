f1a21ee3cd0b9e26d6be1a4c2717fbe77f1dca2b
Ajay Mishra <142102288+shallow090@users.noreply.github.com>
2024-04-14 10:22:10 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -3,7 +3,7 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 
-from bot import Bot
+from bot import Bot1,Bot2
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
